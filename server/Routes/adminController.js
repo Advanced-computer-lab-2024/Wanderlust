bfaeddd138b883bb0567295280d8c6e14e42737c
amgadshehab <114675905+amgadshehab@users.noreply.github.com>
2024-09-29 14:38:47 +0300

    update AdminController
    added the basics

---
+++ server/Routes/adminController.js
@@ -0,0 +1,15 @@
+const adminModel = require('../Models/Admin.js');
+const { default: mongoose } = require('mongoose');
+
+//each one add a method that is needed based on the requirements
+//Create or add a new admin - Amgad
+const createAdmin = async (req, res) => {
+
+
+};
+
+
+
+
+//add a , then the name of ur mehtod below.
+module.exports = {createAdmin};
