a4d2384d4068d4dd20330d8e3db6e2d88c91c833
Khaled <khaledmostafagit@gmail.com>
2024-09-29 18:01:34 +0300

    Finished point 17

--- server/App.js
+++ server/App.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 //Make sure to add your MongoDB URI in the .env file as MONGO_URI="your mongodb uri"
 //Check db connection links in README file
+const {addTourismGovernor} = require("./Routes/adminController");
 const MongoURI =
   "mongodb+srv://alimousa2003:33Dt6AmBI1uV9DG7@mernapp.l0tdo.mongodb.net/?retryWrites=true&w=majority&appName=MernApp";
 
@@ -23,3 +24,7 @@ mongoose
     });
   })
   .catch((err) => console.log(err));
+
+
+app.use(express.json())
+app.post("/admin/addTourismGovernor",addTourismGovernor);
--- server/Routes/adminController.js
+++ server/Routes/adminController.js
@@ -1,5 +1,6 @@
 const adminModel = require('../Models/Admin.js');
 const { default: mongoose } = require('mongoose');
+const tourismGovernorModel = require('../Models/TourismGovernor.js');
 
 //each one add a method that is needed based on the requirements
 //Create or add a new admin - Amgad
@@ -8,8 +9,30 @@ const createAdmin = async (req, res) => {
 
 };
 
+// Create a new tourism governor
+const addTourismGovernor = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // Check if the username already exists
+        const existingGovernor = await tourismGovernorModel.findOne({ username });
+        if (existingGovernor) {
+            return res.status(400).json({ message: 'Username already exists' });
+        }
+
+        // Create a new tourism governor
+        const newGovernor = new tourismGovernorModel({ username, password });
+
+        // Save the new governor to the database
+        await newGovernor.save();
+
+        res.status(201).json({ message: 'Tourism governor created successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+};
 
 
 
 //add a , then the name of ur mehtod below.
-module.exports = {createAdmin};
+module.exports = { createAdmin, addTourismGovernor };
