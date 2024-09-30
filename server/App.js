40f74437dcd122453fdcd269e73c5a17284853f2
nadaa <nada.abdrabou@student.guc.edu.eg>
2024-10-01 00:54:54 +0300

    admin deleting works on all accounts

--- server/App.js
+++ server/App.js
@@ -6,17 +6,11 @@ require("dotenv").config();
 //Make sure to add your MongoDB URI in the .env file as MONGO_URI="your mongodb uri"
 //Check db connection links in README file
 
+//calling for controllers
 const {createTourGuide,getTourGuide, updateTourGuide, deleteTourGuide} = require("./Controllers/tourGuideController");
-
-
 const {createAdvertiser,
   getAdvertiser,
   updateAdvertiser,createActivity, getActivity, updateActivity, deleteActivity} = require("./Controllers/advertiserController");
-
-
-//calling admin controllers
-
-//calling activity category controllers
 const { createCategory, getAllCategories, updateCategory, deleteCategory } = require("./Controllers/ActivityCategoryController");
 const { createAdmin, deleteAccount, getAllUsernames, addTourismGovernor } = require("./Controllers/adminController");
 
@@ -44,36 +38,30 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-
+//routes for tour guide
 app.use(express.json())
 app.post("/createtgprofile",createTourGuide);
 app.get("/gettgprofile", getTourGuide);
 app.put("/updatetgprofile", updateTourGuide);
 app.delete("/deletetgprofile", deleteTourGuide);
-
-
+//routes for advertiser
 app.use(express.json())
 app.post("/createAdvertiserProfile",createAdvertiser);
 app.get("/getAdvertiser", getAdvertiser);
 app.put("/updateAdvertiser", updateAdvertiser);
-
-
+//routes for admin
 app.use(express.json())
-//routes
 app.post("/admin/addTourismGovernor",addTourismGovernor);
 app.delete("/admin/delete", deleteAccount);
 app.get("/admin/usernames", getAllUsernames);
 app.put("/admin/create", createAdmin);
-
-app.use(express.json())
 //routes for activity category
+app.use(express.json())
 app.post("/ActivityCategory/CreateCategory",createCategory);
 app.get("/ActivityCategory/GetAllCategories",getAllCategories);
 app.put("/ActivityCategory/UpdateCategory",updateCategory);
 app.delete("/ActivityCategory/DeleteCategory",deleteCategory);
-
-
-
+//routes for activity
 app.use(express.json())
 app.post('/createActivity', createActivity);
 app.get('/getActivity', getActivity);
