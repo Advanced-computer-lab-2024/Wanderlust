006851b1a5ab27a3d8e269ce1b3d501e7f210328
nadaa <nada.abdrabou@student.guc.edu.eg>
2024-10-06 02:26:51 +0300

    admin frontend

--- server/Controllers/adminController.js
+++ server/Controllers/adminController.js
@@ -31,6 +31,29 @@ const adminLogin = async (req, res) => {
         res.status(500).json({ message: 'Server error', error: error.message });
     }
 };
+
+// Get admin details by username
+const getAdminDetails = async (req, res) => {
+    try {
+        const { username } = req.query;
+        const adminAccount = await adminModel.findOne({ username });
+
+        if (adminAccount) {
+            res.status(200).json({
+                id: adminAccount._id,
+                name: adminAccount.name,
+                email: adminAccount.email,
+                username: adminAccount.username
+            });
+        } else {
+            res.status(404).json({ message: 'Admin not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
+
 const createAdmin = async (req, res) => {
     try {
         const { name, email, password, username } = req.body;
@@ -47,65 +70,64 @@ const createAdmin = async (req, res) => {
 
 };
 
-//deleting account based on the username
+//deleting account based on the id
 const deleteAccount = async (req, res) => {
     try {
-        const { username } = req.body;
-        const adminAccount = await adminModel.findOne({ username });
-        const tourismGovernorAccount = await tourismGovernorModel.findOne({ username });
-        const tourguideAccount = await tourguideModel.findOne({ username });
-        const touristAccount = await touristModel.findOne({ username });
-        const advertiserAccount = await advertiserModel.findOne({ username });
-        const sellerAccount = await sellerModel.findOne({ username }); 
+        const { id } = req.body;
+        const adminAccount = await adminModel.findById(id);
+        const tourismGovernorAccount = await tourismGovernorModel.findById(id);
+        const tourguideAccount = await tourguideModel.findById(id);
+        const touristAccount = await touristModel.findById(id);
+        const advertiserAccount = await advertiserModel.findById(id);
+        const sellerAccount = await sellerModel.findById(id); 
 
-      if (adminAccount) {
-        await adminAccount.deleteOne();
-        res.status(200).json({ message: 'Admin account deleted successfully' });
-    } else if (tourismGovernorAccount) {
-        await tourismGovernorAccount.deleteOne();
-        res.status(200).json({ message: 'Tourism governor account deleted successfully' });
-    } else if (tourguideAccount) {
-        await tourguideAccount.deleteOne();
-        res.status(200).json({ message: 'Tourguide account deleted successfully' });
-    } else if (touristAccount) {
-        await touristAccount.deleteOne();
-        res.status(200).json({ message: 'Tourist account deleted successfully' });
-    } else if (advertiserAccount) {
-        await advertiserAccount.deleteOne();
-        res.status(200).json({ message: 'Advertiser account deleted successfully' });
-    }else if (sellerAccount) { 
-        await sellerAccount.deleteOne();
-        res.status(200).json({ message: 'Seller account deleted successfully' });
-    }else {
-        res.status(404).json({ message: 'Account not found' });
-    }
+        if (adminAccount) {
+            await adminAccount.deleteOne();
+            res.status(200).json({ message: 'Admin account deleted successfully' });
+        } else if (tourismGovernorAccount) {
+            await tourismGovernorAccount.deleteOne();
+            res.status(200).json({ message: 'Tourism governor account deleted successfully' });
+        } else if (tourguideAccount) {
+            await tourguideAccount.deleteOne();
+            res.status(200).json({ message: 'Tourguide account deleted successfully' });
+        } else if (touristAccount) {
+            await touristAccount.deleteOne();
+            res.status(200).json({ message: 'Tourist account deleted successfully' });
+        } else if (advertiserAccount) {
+            await advertiserAccount.deleteOne();
+            res.status(200).json({ message: 'Advertiser account deleted successfully' });
+        } else if (sellerAccount) { 
+            await sellerAccount.deleteOne();
+            res.status(200).json({ message: 'Seller account deleted successfully' });
+        } else {
+            res.status(404).json({ message: 'Account not found' });
+        }
     } catch (error) {
-      res.status(500).json({ message: 'Server error', error });
+        res.status(500).json({ message: 'Server error', error });
     }
-  };
+};
 
 // helper get all usernames on system and account type
-const getAllUsernames = async (req, res) => {
+const getAllUserDetails = async (req, res) => {
     try {
-        const adminAccounts = await adminModel.find({}, 'username role');
-        const tourismGovernorAccounts = await tourismGovernorModel.find({}, 'username role');
-        const tourguideAccounts = await tourguideModel.find({}, 'userName role');
-        const touristAccounts = await touristModel.find({}, 'username role');
-        const advertiserAccounts = await advertiserModel.find({}, 'username role');
-        const sellerAccounts = await sellerModel.find({}, 'username role');
-
-        const accounts = [
-            ...adminAccounts.map(account => ({ username: account.username, accountType: 'admin' })),
-            ...tourismGovernorAccounts.map(account => ({ username: account.username, accountType: 'tourismGovernor' })),
-            ...tourguideAccounts.filter(account => account.userName).map(account => ({ username: account.userName, accountType: 'tourguide' })),
-            ...touristAccounts.map(account => ({ username: account.username, accountType: 'tourist' })),
-            ...advertiserAccounts.map(account => ({ username: account.username, accountType: 'advertiser' })),
-            ...sellerAccounts.map(account => ({ username: account.username, accountType: 'seller' }))
-            
-        ];
+            const adminAccounts = await adminModel.find({}, '_id username email password role');
+            const tourismGovernorAccounts = await tourismGovernorModel.find({}, '_id username email password role');
+            const tourguideAccounts = await tourguideModel.find({}, '_id userName email password role');
+            const touristAccounts = await touristModel.find({}, '_id username email password role');
+            const advertiserAccounts = await advertiserModel.find({}, '_id username email password role');
+            const sellerAccounts = await sellerModel.find({}, '_id username email password role');
+    
+            const accounts = [
+                ...adminAccounts.map(account => ({ id: account._id, username: account.username, email: account.email, password: account.password, accountType: 'admin' })),
+                ...tourismGovernorAccounts.map(account => ({ id: account._id, username: account.username, email: account.email, password: account.password, accountType: 'tourismGovernor' })),
+                ...tourguideAccounts.filter(account => account.userName).map(account => ({ id: account._id, username: account.userName, email: account.email, password: account.password, accountType: 'tourguide' })),
+                ...touristAccounts.map(account => ({ id: account._id, username: account.username, email: account.email, password: account.password, accountType: 'tourist' })),
+                ...advertiserAccounts.map(account => ({ id: account._id, username: account.username, email: account.email, password: account.password, accountType: 'advertiser' })),
+                ...sellerAccounts.map(account => ({ id: account._id, username: account.username, email: account.email, password: account.password, accountType: 'seller' }))
+            ];
         res.status(200).json(accounts);
     } catch (error) {
-        console.error('Error fetching usernames:', error); 
+        console.error('Error fetching user details:', error); 
         res.status(500).json({ message: 'Server error', error: error.message });
     }
 };
@@ -136,4 +158,4 @@ const addTourismGovernor = async (req, res) => {
 
 
 //add a , then the name of ur mehtod below.
-module.exports = { createAdmin, addTourismGovernor, deleteAccount, getAllUsernames, adminLogin };
+module.exports = { createAdmin, addTourismGovernor, deleteAccount, getAllUserDetails, adminLogin, getAdminDetails };
