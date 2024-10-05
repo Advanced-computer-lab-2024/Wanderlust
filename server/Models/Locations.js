561d370233450b2af09eea8530fe04a76847e40c
AliMousa11 <alimousa2003@icloud.com>
2024-10-05 20:41:00 +0300

    started touristLists.html

--- server/Models/Locations.js
+++ server/Models/Locations.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const locationsSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  pictures: [{ data: Buffer, contentType: String }],
+  pictures: { data: Buffer, contentType: String },
   location: { type: String, required: true },
   openingHours: { type: String, required: true },
   ticketPrices: { type: Number, required: true },
