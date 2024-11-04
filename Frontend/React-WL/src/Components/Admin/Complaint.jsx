a4b4710955acb6eb88512250c5c249886b85070b
amrkhaled617 <amrkhaled54321@outlook.com>
2024-11-04 21:32:10 +0200

    update complaint status

--- Frontend/React-WL/src/Components/Admin/Complaint.jsx
+++ Frontend/React-WL/src/Components/Admin/Complaint.jsx
@@ -22,6 +22,23 @@ const ViewComplaint = () => {
         setComplaint(complaint);
     }
 
+    const changeStatus = async () => {
+        const token = localStorage.getItem('jwtToken');
+        const updatedStatus = complaint.status === 'resolved' ? 'pending' : 'resolved';
+        const response = await axios.put('http://localhost:8000/api/Complaint/complaint/status', 
+            { id: complaintId, status: updatedStatus }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        if (response.status === 200) {
+            setComplaint({ ...complaint, status: updatedStatus });
+            alert('Status updated successfully');
+        } else {
+            alert('Failed to update status: ' + response.data.message);
+        }
+    }
+
     return(
         <>
         <Navbar t1={"Admin Dashboard"} p1={"/admindashboard"} t6={"Login"} p6={"/Login"}/>
@@ -30,13 +47,13 @@ const ViewComplaint = () => {
                 <h2 className='text-2xl font-bold mb-6'>Complaint Details</h2>
             </div>
             <div className="d-flex flex-wrap gap-3">     
-                    <div key={complaint.id} className="flex-fill border rounded p-4 shadow-sm" style={{ flex: '1 1 calc(33.333% - 20px)' }}>
+                    <div key={complaint.id} className="flex-fill border rounded p-4 shadow-sm mb-4" style={{ flex: '1 1 calc(33.333% - 20px)' }}>
                         <span ><span className='text-gray-800 font-semibold'>Title:</span> {complaint.title}</span><br />
                         <span><span className='text-gray-800 font-semibold'>status:</span> {complaint.status}</span><br />
                         <span><span className='text-gray-800 font-semibold'>Body:</span> {complaint.body}</span><br />
                         <span><span className='text-gray-800 font-semibold'>Reply:</span> {complaint.adminReply}</span><br />
-                        <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white mt-2">Reply</button>
-
+                        <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white mt-2 mr-2">Reply</button>
+                        <button onClick={changeStatus} className="btn bg-indigo-500 hover:bg-indigo-600 text-white mt-2">Change Status</button>
                     </div>
             </div>
         </div>
