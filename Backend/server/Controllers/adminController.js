01cfaa6ed6e4962d737370cc6a6869f4643e5fae
AliMousa11 <alimousa2003@icloud.com>
2024-11-08 16:43:47 +0200

    modified getPendingUsers

--- Backend/server/Controllers/adminController.js
+++ Backend/server/Controllers/adminController.js
@@ -1,125 +1,165 @@
-const adminModel = require('../Models/Admin.js');
-const tourismGovernorModel = require('../Models/TourismGovernor.js');
-const tourguideModel = require('../Models/tourGuide'); 
-const touristModel = require('../Models/Tourist.js');
-const advertiserModel = require('../Models/Advertiser'); 
-const sellerModel = require('../Models/Seller.js');
-const User = require('../Models/user');
+const adminModel = require("../Models/Admin.js");
+const tourismGovernorModel = require("../Models/TourismGovernor.js");
+const tourguideModel = require("../Models/tourGuide");
+const touristModel = require("../Models/Tourist.js");
+const advertiserModel = require("../Models/Advertiser");
+const sellerModel = require("../Models/Seller.js");
+const User = require("../Models/user");
 //npm install jsonwebtoken
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // Get admin details by username
 const getAdminDetails = async (req, res) => {
-    try {
-        const { username } = req.query;
-        const adminAccount = await adminModel.findOne({ username });
+  try {
+    const { username } = req.query;
+    const adminAccount = await adminModel.findOne({ username });
 
-        if (adminAccount) {
-            res.status(200).json({
-                id: adminAccount._id,
-                name: adminAccount.name,
-                email: adminAccount.email,
-                username: adminAccount.username
-            });
-        } else {
-            res.status(404).json({ message: 'Admin not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'Server error', error: error.message });
+    if (adminAccount) {
+      res.status(200).json({
+        id: adminAccount._id,
+        name: adminAccount.name,
+        email: adminAccount.email,
+        username: adminAccount.username,
+      });
+    } else {
+      res.status(404).json({ message: "Admin not found" });
     }
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
-
 const createAdmin = async (req, res) => {
-    try {
-        const { name, email, password, username } = req.body;
-        const existingAdmin = await adminModel.findOne({ username });
-        if (existingAdmin) {
-            return res.status(400).json({ message: 'Username already exists' });
-        }
-        const newAdmin = new adminModel({ name, email, password, username });
-        await newAdmin.save();
-        res.status(201).json({ message: 'Admin created successfully' });
-    } catch (error) {
-        console.error('Error creating admin:', error);
-        res.status(500).json({ message: 'Server error', error });
+  try {
+    const { name, email, password, username } = req.body;
+    const existingAdmin = await adminModel.findOne({ username });
+    if (existingAdmin) {
+      return res.status(400).json({ message: "Username already exists" });
     }
+    const newAdmin = new adminModel({ name, email, password, username });
+    await newAdmin.save();
+    res.status(201).json({ message: "Admin created successfully" });
+  } catch (error) {
+    console.error("Error creating admin:", error);
+    res.status(500).json({ message: "Server error", error });
+  }
 };
 
 //deleting account based on the id
 const deleteAccount = async (req, res) => {
-    const { id } = req.body;
-  
-    try {
-      let account = await adminModel.findById(id) ||
-                    await tourismGovernorModel.findById(id) ||
-                    await tourguideModel.findById(id) ||
-                    await touristModel.findById(id) ||
-                    await advertiserModel.findById(id) ||
-                    await sellerModel.findById(id) ||
-                    await User.findById(id);
-  
-      if (!account) {
-        return res.status(404).json({ message: 'Account not found' });
-      }
-  
-      await account.deleteOne();
-      res.status(200).json({ message: 'Account deleted successfully' });
-    } catch (error) {
-      res.status(500).json({ message: 'Server error', error });
+  const { id } = req.body;
+
+  try {
+    let account =
+      (await adminModel.findById(id)) ||
+      (await tourismGovernorModel.findById(id)) ||
+      (await tourguideModel.findById(id)) ||
+      (await touristModel.findById(id)) ||
+      (await advertiserModel.findById(id)) ||
+      (await sellerModel.findById(id)) ||
+      (await User.findById(id));
+
+    if (!account) {
+      return res.status(404).json({ message: "Account not found" });
     }
-  };
+
+    await account.deleteOne();
+    res.status(200).json({ message: "Account deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
 
 // helper get all usernames on system and account type
 const getAllUserDetails = async (req, res) => {
-    try {
-        const adminAccounts = await adminModel.find({}, '_id username email password role');
-        const tourismGovernorAccounts = await tourismGovernorModel.find({}, '_id username email password role');
-        const userAccounts = await User.find({}, '_id username email password role');
+  try {
+    const adminAccounts = await adminModel.find(
+      {},
+      "_id username email password role"
+    );
+    const tourismGovernorAccounts = await tourismGovernorModel.find(
+      {},
+      "_id username email password role"
+    );
+    const userAccounts = await User.find(
+      {},
+      "_id username email password role"
+    );
 
-        const accounts = [
-            ...adminAccounts.map(account => ({ id: account._id, username: account.username, email: account.email, password: account.password, accountType: 'admin' })),
-            ...tourismGovernorAccounts.map(account => ({ id: account._id, username: account.username, email: account.email, password: account.password, accountType: 'tourismGovernor' })),
-            ...userAccounts.map(account => ({id: account._id, username: account.username, email: account.email, password: account.password, accountType: account.role })),
-        ];
-        res.status(200).json(accounts);
-    } catch (error) {
-        res.status(500).json({ message: 'Server error', error });
-    }
+    const accounts = [
+      ...adminAccounts.map((account) => ({
+        id: account._id,
+        username: account.username,
+        email: account.email,
+        password: account.password,
+        accountType: "admin",
+      })),
+      ...tourismGovernorAccounts.map((account) => ({
+        id: account._id,
+        username: account.username,
+        email: account.email,
+        password: account.password,
+        accountType: "tourismGovernor",
+      })),
+      ...userAccounts.map((account) => ({
+        id: account._id,
+        username: account.username,
+        email: account.email,
+        password: account.password,
+        accountType: account.role,
+      })),
+    ];
+    res.status(200).json(accounts);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
 };
 
 // Get all pending users
 const getPendingUsers = async (req, res) => {
-    try {
-        const pendingUsers = await User.find({ roleApplicationStatus: 'pending' });
-        res.status(200).json(pendingUsers);
-    } catch (error) {
-        res.status(500).json({ message: 'Server error', error: error.message });
-    }
+  try {
+    const pendingAdvertisers = await advertiserModel
+      .find({ roleApplicationStatus: "pending" })
+      .populate("userId");
+    const pendingSellers = await sellerModel
+      .find({ roleApplicationStatus: "pending" })
+      .populate("userId");
+    const pendingTourGuides = await tourguideModel
+      .find({ roleApplicationStatus: "pending" })
+      .populate("userId");
+    res.status(200).json({
+      pendingAdvertisers,
+      pendingSellers,
+      pendingTourGuides,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
 // Create a new tourism governor
 const addTourismGovernor = async (req, res) => {
-    try {
-        const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-        // Check if the username already exists
-        const existingGovernor = await tourismGovernorModel.findOne({ username });
-        if (existingGovernor) {
-            return res.status(400).json({ message: 'Username already exists' });
-        }
-        const newGovernor = new tourismGovernorModel({ username, password });
-        await newGovernor.save();
-        res.status(201).json({ message: 'Tourism governor created successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'Server error', error });
+    // Check if the username already exists
+    const existingGovernor = await tourismGovernorModel.findOne({ username });
+    if (existingGovernor) {
+      return res.status(400).json({ message: "Username already exists" });
     }
+    const newGovernor = new tourismGovernorModel({ username, password });
+    await newGovernor.save();
+    res.status(201).json({ message: "Tourism governor created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
 };
 
-
-module.exports = { createAdmin, 
-                    addTourismGovernor, 
-                    deleteAccount, 
-                    getAllUserDetails, 
-                    getAdminDetails,
-                    getPendingUsers};
+module.exports = {
+  createAdmin,
+  addTourismGovernor,
+  deleteAccount,
+  getAllUserDetails,
+  getAdminDetails,
+  getPendingUsers,
+};
