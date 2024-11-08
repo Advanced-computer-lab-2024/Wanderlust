1460f45915186bea0e64f2d2e36d6f3c3fd96dd9
amrkhaled617 <amrkhaled54321@outlook.com>
2024-11-08 20:37:13 +0200

    TourismGovernorProfile and remove home from navbar

--- Frontend/React-WL/src/Pages/TourismGovernor/TourismGovrernorProfilePage.jsx
+++ Frontend/React-WL/src/Pages/TourismGovernor/TourismGovrernorProfilePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import TourismGovernorNavBar from '../../Components/NavBars/TourismGovernorNavBar'
+import TourismGovernorProfile from '../../Components/TourismGovernorProfile';
 
 const TourismGovernorProfilePage = () => {
     return (
     <>      
       <TourismGovernorNavBar /> 
       {/* Tourism governor Profile insert here */}
+      <TourismGovernorProfile />
     </>
     )
   }
