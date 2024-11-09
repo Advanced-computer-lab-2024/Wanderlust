6a674790d529559ce051c0f5b564a5b298e39d9a
AliMousa11 <alimousa2003@icloud.com>
2024-11-09 16:52:21 +0200

    implemented approvePendingUser,needs testing

--- Backend/server/Routes/adminRoutes.js
+++ Backend/server/Routes/adminRoutes.js
@@ -1,22 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const adminController = require('../Controllers/adminController');
-const { login, updatePassword, authenticateUser, getLoggedInUser , getLoggedInInfo , acceptTerms} = require('../Controllers/authController');
+const adminController = require("../Controllers/adminController");
+const {
+  login,
+  updatePassword,
+  authenticateUser,
+  getLoggedInUser,
+  getLoggedInInfo,
+  acceptTerms,
+} = require("../Controllers/authController");
 
-router.use(express.json())
-router.post("/addTourismGovernor",adminController.addTourismGovernor);
+router.use(express.json());
+router.post("/addTourismGovernor", adminController.addTourismGovernor);
 router.delete("/delete", adminController.deleteAccount);
 router.get("/usernames", adminController.getAllUserDetails);
 router.post("/create", adminController.createAdmin);
 router.get("/pendingUsers", adminController.getPendingUsers);
+router.put("/approvePendingUser/:userId", adminController.approvePendingUser);
 router.get("/profile", adminController.getAdminDetails);
 //router.post("/updatepassword", adminController.authenticateAdmin,adminController.updateAdminPassword);
 
-router.post('/login', login);
-router.post('/updatepassword', authenticateUser, updatePassword);
-router.get('/getLoggedInUser', getLoggedInUser);
-router.get('/getLoggedInInfo', getLoggedInInfo);
-router.post('/acceptTerms', acceptTerms);
-
+router.post("/login", login);
+router.post("/updatepassword", authenticateUser, updatePassword);
+router.get("/getLoggedInUser", getLoggedInUser);
+router.get("/getLoggedInInfo", getLoggedInInfo);
+router.post("/acceptTerms", acceptTerms);
 
 module.exports = router;
