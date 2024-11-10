ceb5ac1fba3eeab100cd0a3710f00e04cc530e9e
Khaled Mostafa <khaledmostafagit@gmail.com>
2024-11-10 21:06:47 +0200

    minor change in 2

--- Frontend/React-WL/src/Components/Admin/UpdateAdminPassword.jsx
+++ Frontend/React-WL/src/Components/Admin/UpdateAdminPassword.jsx
@@ -20,7 +20,7 @@ const UpdateAdminPassword = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.put('http://localhost:8000/api/admin/updatepassword', formData, {
+            const response = await axios.post('http://localhost:8000/api/admin/updatepassword', formData, {
                 headers: {
                     Authorization: `Bearer ${localStorage.getItem('jwtToken')}`
                 }
