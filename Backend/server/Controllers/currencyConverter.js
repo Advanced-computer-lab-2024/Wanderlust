7d2ed32682c0f944b00208cede4011a4cd3bebd0
yousefmo22 <y.yousef.moh@gmail.com>
2024-11-12 01:41:28 +0200

    new currency api key for evaluation

--- Backend/server/Controllers/currencyConverter.js
+++ Backend/server/Controllers/currencyConverter.js
@@ -3,7 +3,7 @@ const Tourist = require('../Models/Tourist');
 
 const getExchangeRates = async (baseCurrency = 'EGP') => {
   try {
-    const response = await axios.get(`https://v6.exchangerate-api.com/v6/24854109d0f837a5bd8882e3/latest/${baseCurrency}`);
+    const response = await axios.get(`https://v6.exchangerate-api.com/v6/83dd3be897b633310e44ab9f/latest/${baseCurrency}`);
     return response.data.conversion_rates;
   } catch (error) {
     throw new Error('Failed to fetch exchange rates');
